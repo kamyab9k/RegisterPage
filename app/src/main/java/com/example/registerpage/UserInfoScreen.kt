1156e6e9c99ae4c607d920e16117f4d220b7c2f2
kamyab khsvi <khosravi036@gmail.com>
2023-12-01 19:37:37 +0330

    UserInfoScreen ui clean up

--- app/src/main/java/com/example/registerpage/UserInfoScreen.kt
+++ app/src/main/java/com/example/registerpage/UserInfoScreen.kt
@@ -3,18 +3,26 @@ package com.example.registerpage
 import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+
 
 @Composable
 fun UserInfoScreen(
@@ -30,21 +38,62 @@ fun UserInfoScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.Start
+            .padding(16.dp)
     ) {
+        Spacer(modifier = Modifier.height(64.dp))
+        Text(
+            text = "User Information",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(1),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            item {
+                CustomDivider()
+            }
+            item {
+                DataItem("Name : ", name.orEmpty())
+            }
+            item {
+                CustomDivider()
+            }
 
-        Text(text = "Your Information", fontSize = 32.sp)
+            item {
+                DataItem("Last Name : ", lastName.orEmpty())
+            }
+            item {
+                CustomDivider()
+            }
+            item {
+                DataItem("ID Number : ", id.orEmpty())
+            }
+            item {
+                CustomDivider()
+            }
+            item {
+                DataItem("Picked Date : ", pickedDate.orEmpty())
+            }
+            item {
+                CustomDivider()
+            }
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
 
-        Text(text = "Name: $name", fontSize = 16.sp)
-
-        Text(text = "LastName: $lastName", fontSize = 16.sp)
-
-        Text(text = "ID: $id", fontSize = 16.sp)
-
-        Text(text = "PickedDate: $pickedDate", fontSize = 16.sp)
-
+        ) {
+        Spacer(modifier = Modifier.height(128.dp))
         Button(
             onClick = {
                 userSessionManager.clearSession()
@@ -53,10 +102,10 @@ fun UserInfoScreen(
             },
             modifier = Modifier
                 .fillMaxWidth()
-
         ) {
-            Text(text = "Log Out")
+            Text(text = "Log Out", modifier = Modifier.padding(start = 8.dp))
         }
+
         Button(
             onClick = {
                 (context as? Activity)?.finish()
@@ -64,9 +113,30 @@ fun UserInfoScreen(
             },
             modifier = Modifier
                 .fillMaxWidth()
-
+                .padding(top = 8.dp)
         ) {
-            Text(text = "Exit")
+            Text(text = "Exit", modifier = Modifier.padding(start = 8.dp))
         }
     }
+}
+
+@Composable
+fun DataItem(fieldName: String, value: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(text = fieldName, fontWeight = FontWeight.Bold)
+        Text(text = value)
+    }
+}
+
+@Composable
+fun CustomDivider() {
+    Divider(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+    )
 }
--- app/src/main/java/com/example/registerpage/UserInformation.kt
+++ app/src/main/java/com/example/registerpage/UserInformation.kt
@@ -2,7 +2,7 @@ package com.example.registerpage
 
 // no use for now
 data class UserInformation(
-    var name: String?, var lastName: String?, var id: String?, var pickedDate: String?
-){
+    var name: String?, var lastName: String?, var id: String?, var pickedDate: String?,
+) {
 
 }
